1be375fb5085f9256f5afce4257fe2c2768b5e4f
Jakob Sundstrom <jakob.sundstrom1@gmail.com>
2023-07-14 16:43:11 +0200

    delete everything and find solution on stackoverfl

--- js-script.js
+++ js-script.js
@@ -1,60 +1,13 @@
-let isPaused = false;
-let isEraserEnabled = false;
+const container = document.getElementById("container");
 
-function generateSquares() {
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
 
-    if (isPaused){
-        return;
-    }
-
-    let container = document.querySelector('#container');
-  
-    for (let i = 0; i < 256; i++) {
-      const content = document.createElement('div');
-      content.classList.add('square');
-  
-      // Add event listeners for hover effect
-      content.addEventListener('mouseenter', function() {
-        content.classList.add('hovered');
-      });
-  
-      container.appendChild(content);
-    }
-
-  }
-
-  function eraserFunc() {
-    isPaused = true;
-    isEraserEnabled = true;
-
-    let hoveredSquares = document.querySelectorAll('.hovered');
-    hoveredSquares.forEach((square) => {
-      square.addEventListener('mouseenter', function() {
-        square.classList.remove('hovered');
-      });
-    });
-  }
-
-  function newSketchFunc(){
-        let hoveredSquares = document.querySelectorAll('.hovered');
-        hoveredSquares.forEach((square) => {   
-            square.classList.remove('hovered');
-    });
-  }
-
-  /*buttons */
-  const start = document.querySelector('#start');
-    start.addEventListener('click', () =>{
-        generateSquares();
-    });
-
-  const eraser = document.querySelector('#eraser');
-    eraser.addEventListener('click', () =>{
-        eraserFunc();
-    });
-
-    const newSketch = document.querySelector('#newSketch');
-        newSketch.addEventListener('click', ()  =>{
-            newSketchFunc();
-    });
-
+makeRows(16, 16);
