168fc7d95669b8ac4141499a9da85c6639560e7c
Jakob Sundstrom <jakob.sundstrom1@gmail.com>
2023-07-14 20:11:41 +0200

    add button asking for number of rows/cols

--- js-script.js
+++ js-script.js
@@ -5,9 +5,35 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    cell.classList.add('grid-item')
+    container.appendChild(cell)
+
+    cell.addEventListener('mouseenter', function(){
+        cell.classList.add('hovered');
+    });
+    
   };
+
 };
 
 makeRows(16, 16);
+
+
+
+const sizebtn = document.querySelector('#sizeBtn');
+    sizebtn.addEventListener('click', () => {
+        container.innerHTML = '';
+
+        var x = prompt("Enter rows: ", "16")
+        var y = prompt("Enter columns: ", "16")
+
+        var num1 = parseInt(x)
+        var num2 = parseInt(y)
+
+        if ((num1 || num2) > 100){
+            alert("Number too high!")
+        }
+
+        makeRows(num1,num2);
+
+    });
