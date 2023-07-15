ce603852ae4e7021ebbe571689dae4d632a6a5a5
Jakob Sundstrom <jakob.sundstrom1@gmail.com>
2023-07-15 22:12:48 +0200

    add eraser function

--- js-script.js
+++ js-script.js
@@ -34,6 +34,7 @@ const reSketch = document.querySelector('#reSketch');
 
 makeRows(16, 16);
 
+/*create eraser*/
 
 
 /*random color */
@@ -163,3 +164,20 @@ const columnSlider = document.getElementById('columnSlider');
 
 rowSlider.addEventListener('input', setGridSize);
 columnSlider.addEventListener('input', setGridSize);
+
+
+function eraserFunc() {
+  const cells = document.querySelectorAll('.grid-item');
+
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', function () {
+      cell.style.backgroundColor = "transparent";
+    });
+  });
+}
+
+const eraserBtn = document.querySelector('#eraserBtn');
+  eraserBtn.addEventListener('click', () => {
+    eraserFunc();
+  });
+
