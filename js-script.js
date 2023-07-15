cafd6c735d02821f18bd12c22c807eb55cffaa5d
JakobSundstrom <jakob.sundstrom1@gmail.com>
2023-07-15 11:51:13 +0200

    add random color and rgb buttons and function

--- js-script.js
+++ js-script.js
@@ -5,18 +5,26 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.classList.add('grid-item')
-    container.appendChild(cell)
+    cell.classList.add('grid-item');
+    container.appendChild(cell);
+  }
+}
 
-    cell.addEventListener('mouseenter', function(){
-        cell.classList.add('hovered');
+function startPainting() {
+  const cells = document.querySelectorAll('.grid-item');
+
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', function () {
+      cell.classList.add('hovered');
     });
-    
-  };
+  });
+}
 
-};
+const paintBtn = document.querySelector('#paintBtn');
+paintBtn.addEventListener('click', startPainting);
 
 makeRows(16, 16);
+
 
 
 
@@ -37,3 +45,54 @@ const sizebtn = document.querySelector('#sizeBtn');
         makeRows(num1,num2);
 
     });
+
+/*random color */
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function paintRandColor(){
+
+  const cells = document.querySelectorAll('.grid-item');
+
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', function () {
+      cell.style.backgroundColor = getRandomColor();
+    });
+  });
+}
+
+const randColorBtn = document.querySelector('#randColorBtn');
+    randColorBtn.addEventListener('click', () => {
+      paintRandColor();
+    });
+
+
+/*rgb color */
+function getRgbColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function paintRbg(){
+
+  const cells = document.querySelectorAll('.grid-item');
+
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', function () {
+      cell.style.backgroundColor = getRgbColor();
+    });
+  });
+}
+
+const rbgBtn = document.querySelector('#rgbBtn');
+  rbgBtn.addEventListener('click', () => {
+    paintRbg();
+  });
+
+
+
